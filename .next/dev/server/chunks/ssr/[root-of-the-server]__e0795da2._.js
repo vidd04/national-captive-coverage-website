7fe75f8d7697a9162b22469646ca540a19668131
vidhur senthil <vidhursenthil@gmail.com>
2025-12-16 02:26:00 -0500

    third

--- .next/dev/server/chunks/ssr/[root-of-the-server]__e0795da2._.js
+++ .next/dev/server/chunks/ssr/[root-of-the-server]__e0795da2._.js
@@ -3041,7 +3041,7 @@ function Home() {
                     children: [
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Documents$2f$national$2d$captive$2d$coverage$2d$website$2f$node_modules$2f2e$pnpm$2f$next$40$16$2e$0$2e$7_react$2d$dom$40$19$2e$2$2e$0_react$40$19$2e$2$2e$0_$5f$react$40$19$2e$2$2e$0$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                             className: "font-heading text-2xl sm:text-3xl text-foreground mb-4",
-                            children: "Placeholder Section (hi)"
+                            children: "Placeholder Section"
                         }, void 0, false, {
                             fileName: "[project]/Documents/national-captive-coverage-website/app/page.tsx",
                             lineNumber: 43,
@@ -3049,7 +3049,7 @@ function Home() {
                         }, this),
                         /*#__PURE__*/ (0, __TURBOPACK__imported__module__$5b$project$5d2f$Documents$2f$national$2d$captive$2d$coverage$2d$website$2f$node_modules$2f2e$pnpm$2f$next$40$16$2e$0$2e$7_react$2d$dom$40$19$2e$2$2e$0_react$40$19$2e$2$2e$0_$5f$react$40$19$2e$2$2e$0$2f$node_modules$2f$next$2f$dist$2f$server$2f$route$2d$modules$2f$app$2d$page$2f$vendored$2f$rsc$2f$react$2d$jsx$2d$dev$2d$runtime$2e$js__$5b$app$2d$rsc$5d$__$28$ecmascript$29$__["jsxDEV"])("p", {
                             className: "text-foreground/70",
-                            children: "This is a temporary section at the bottom of the page. Replace this content with your final copy and layout. Hi."
+                            children: "This is a temporary section at the bottom of the page. Replace this content with your final copy and layout."
                         }, void 0, false, {
                             fileName: "[project]/Documents/national-captive-coverage-website/app/page.tsx",
                             lineNumber: 46,
